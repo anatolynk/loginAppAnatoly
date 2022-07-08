a8b866647f082959f07b50e52e25160b8116c448
Anatoly <anatoly.nk@gmail.com>
2022-07-08 23:00:18 +0300

    added PasswordChangedScreen

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import CreateNewPassword from './app/screens/CreateNewPassword';
 import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import PasswordChangedScreen from './app/screens/PasswordChangedScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 const App = () => {
-  return <CreateNewPassword />;
+  return <PasswordChangedScreen />;
+  // return <CreateNewPassword />;
   // return <ForgotPasswordScreen />;
   // return <RegisterScreen />;
   // return <LoginScreen />;
---
+++ app/screens/PasswordChangedScreen.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import AppButton from '../components/AppButton';
+import AppLink from '../components/AppLink';
+import AppText from '../components/AppText';
+import AppTextInput from '../components/AppTextInput';
+import AppTitle from '../components/AppTitle';
+import Screen from '../components/Screen';
+import themeColors from '../config/themeColors';
+
+import AppIcon from '../components/AppIcon';
+import AppButtonIcon from '../components/AppButtonIcon';
+import AppBackIcon from '../components/AppBackIcon';
+
+function PasswordChangedScreen(props) {
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <View style={styles.layoutContainer}>
+          <AppIcon
+            style={styles.icon}
+            name="shield-checkmark"
+            size={100}
+            color={themeColors.green}
+          />
+          <View style={styles.title}>
+            <AppTitle>Password Changed!</AppTitle>
+            <AppText>Your password has been changed successfully.</AppText>
+          </View>
+          <View style={styles.buttonContainer}>
+            <AppButton
+              title="Back to Login"
+              onPress={() => console.log('Back to Login')}
+            />
+          </View>
+        </View>
+      </View>
+    </Screen>
+  );
+}
+
+export default PasswordChangedScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {},
+  buttonContainer: {},
+  linkContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 26,
+  },
+  title: {
+    marginTop: 35,
+    marginBottom: 28,
+    alignItems: 'center',
+  },
+  layoutContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: -100,
+  },
+  icon: {
+    alignSelf: 'center',
+  },
+});

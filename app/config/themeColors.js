e4fc53a0845111c514b5eb76c22b918f98679cf9
Anatoly <anatoly.nk@gmail.com>
2022-07-08 17:30:20 +0300

    setup theme colors

---
+++ app/config/themeColors.js
@@ -0,0 +1,31 @@
+export default {
+  primary: '#35C2C1',
+  secondary: '#EC727A',
+  black: '#000',
+  white: '#fff',
+
+  dark: '#1E232C',
+  darkGrey: '#6A707C',
+  grey: '#8391A1',
+  border: '#E8ECF4',
+  background: '#F5F5F5',
+  green: '#18C07A',
+  backgroundInput: '#F7F8F9',
+
+  // medium: '#6e6969',
+
+  // black: '#000',
+  // white: '#fff',
+  // dark: '#1B1B1D',
+  // lightGrey: '#F3F3F6',
+  // grey: '#7B7B80',
+
+  // blue: '#2065EB',
+  // red: '#F6004E',
+  // stone: '#FBF7F3',
+
+  // lightGreen: '#42D490',
+  // darkGreen: '#32612E',
+  // lightPurple: '#CE63A7',
+  // lightRed: '#EC727A',
+};
